6f899b2585114b676b5c056643a47449e2fb7f04
Saifalikhan12 <saifalikhan8147881@gmail.com>
2022-03-16 15:08:06 -0700

    MODIFIED: Set Player Initial Position

--- src/com/bridgelabz/snakeandladderproblem/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladderproblem/SnakeAndLadder.java
@@ -1,8 +1,23 @@
 package com.bridgelabz.snakeandladderproblem;
 
 public class SnakeAndLadder {
+
+    //Declaring Variables for Players Position
+    int playerPosition = 0;
+
+    //Method to Show Player Current Position
+    private void showPosition() {
+        //Displaying Player Position
+        System.out.println("Player Starting Position = "+ playerPosition);
+    }
     public static void main(String[] args) {
-    //Displaying Welcome Message
-    System.out.println("Welcome to Snake and Ladder Simulator Problem Developed.");
+        //Displaying Welcome Message
+        System.out.println("Welcome to Snake and Ladder Simulator Problem Developed.");
+
+        //Creating Object for Player
+        SnakeAndLadder player1 = new SnakeAndLadder();
+
+        //Showing Player Position
+        player1.showPosition();
     }
 }
